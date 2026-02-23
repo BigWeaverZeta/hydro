ec0c3a383075ed691277be1280b123be598c33bb
root <root@ip-10-0-155-168.us-east-1.compute.internal>
2026-02-23 20:38:15 +0000

    feat: remove hydro_test crate (moved to hydro-deps)
    - Remove hydro_test from workspace members list
    - Delete hydro_test directory as it has been moved to hydro-deps repo
    
    Co-authored-by: Ilya Elterman <116830922+ilya-amazon@users.noreply.github.com>

--- hydro_test/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    hydro_build_utils::emit_nightly_configuration!();
-
-    stageleft_tool::gen_final!();
-}
--- hydro_test/src/external_client/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod chat;
-pub mod echo;
-pub mod http_counter;
-pub mod http_hello;
